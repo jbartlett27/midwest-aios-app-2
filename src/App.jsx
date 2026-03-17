417a086929fb71856638f77e72605f92ed75aa59
jbartlett27 <jeffreybartlett@me.com>
2026-03-17 06:18:32 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { db } from "./supabase.js";
 class ErrorBoundary extends React.Component {
   constructor(props){super(props);this.state={hasError:false,error:null}}
   static getDerivedStateFromError(error){return {hasError:true,error}}
